d342398921503168de1005cb0c6d4872ae14aa61
DuncanRitchie <duncanritchie@btinternet.com>
2019-04-09 14:39:19 +0100

    Initial Commit

---
+++ Single-player Dice Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Single-player Dice Game
+//
+//  Created by Code Nation 21 on 09/04/2019.
+//  Copyright © 2019 Code Nation 21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Single-player Dice GameTests/Single_player_Dice_GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  Single_player_Dice_GameTests.swift
+//  Single-player Dice GameTests
+//
+//  Created by Code Nation 21 on 09/04/2019.
+//  Copyright © 2019 Code Nation 21. All rights reserved.
+//
+
+import XCTest
+@testable import Single_player_Dice_Game
+
+class Single_player_Dice_GameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
