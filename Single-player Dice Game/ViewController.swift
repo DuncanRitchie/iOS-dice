9ad037998ed3b867b72a47f8859df4bd5c42c9be
DuncanRitchie <duncanritchie@btinternet.com>
2019-04-10 12:16:46 +0100

    Doesn't work, but seems close to working.

--- Single-player Dice Game/ViewController.swift
+++ Single-player Dice Game/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+  
+    @IBOutlet weak var singleDice: UIImageView!
+    @IBOutlet weak var message: UILabel!
+    
+    @IBOutlet weak var currScore: UILabel!
+    
+    var allDice = [#imageLiteral(resourceName: "dice1"),#imageLiteral(resourceName: "dice2"),#imageLiteral(resourceName: "dice3"),#imageLiteral(resourceName: "dice4"),#imageLiteral(resourceName: "dice5"),#imageLiteral(resourceName: "dice6")]
+    var rand: Int = 0
+    var totalScore: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        message.text = ""
+        rand = Int.random(in: 0 ... 5)
+        singleDice.image = allDice[rand]
+        totalScore +=  rand + 1
+        if (rand == 0) {
+            message.text = "You rolled a one! You lose!"
+            totalScore = 0
+        }
+        else if (totalScore >= 21) {
+            message.text = "You got to 21! You win!"
+        }
+        currScore.text = "\(totalScore)"
+    }
+    
+//  The following code was from when the die was rolled on a button press.
+//    @IBAction func rollSingleDie(_ sender: Any) {
+//    }
+    
+    @IBAction func resetSingle(_ sender: Any) {
+        message.text = ""
+        singleDice.image = allDice[1]
+        totalScore = 0
+        currScore.text = "0"
+    }
 }
 
